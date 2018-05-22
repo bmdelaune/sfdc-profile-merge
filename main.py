1e5ec6d744fb0cea7757c4f9dad06630e6732f41
Bradley Delaune <bradley@delaune.me>
2018-05-22 13:17:40 -0500

    supports directories

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sfdc_profile_merge as profile
+from os.path import isdir, isfile, join
+from os import listdir
+from shutil import copyfile
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -11,5 +14,29 @@ parser.add_argument('-o', action='store', dest='out_file_name',
 
 args = parser.parse_args()
 
-profile.mergetheseprofiles(
-    args.input_file_path, args.original_file_path, args.out_file_name)
+if isdir(args.input_file_path):
+    if isdir(args.original_file_path):
+        infiles = [f for f in listdir(
+            args.input_file_path) if isfile(join(args.input_file_path, f))]
+        infiles = list(filter(lambda x: x.endswith('.profile'), infiles))
+        outfiles = [f for f in listdir(
+            args.original_file_path) if isfile(join(args.original_file_path, f))]
+        outfiles = list(filter(lambda x: x.endswith('.profile'), outfiles))
+        filestoprocess = []
+        for infile in infiles:
+            if infile not in outfiles:
+                #TODO copy the file into output directory
+                copyfile(join(args.input_file_path, infile), join(
+                    args.original_file_path, infile))
+            else :
+                profile.mergetheseprofiles(
+                    join(args.input_file_path, infile), join(
+                        args.original_file_path, infile), join(args.original_file_path, infile)) # should probably use output file directory if given
+        
+    else:
+        print('your input was a directory, but the original was a file. Please specifiy a directory')
+else:
+    if args.out_file_name == None:
+        args.out_file_name = args.original_file_path
+
+    profile.mergetheseprofiles(args.input_file_path, args.original_file_path, args.out_file_name)
